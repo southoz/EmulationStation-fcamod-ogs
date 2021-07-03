5e11c484833d165a7e44ce4feba236b98b0a5552
JuanMiguelBG <bacojmbg@gmail.com>
2021-07-03 16:32:35 +0200

    Fixes #10 - Remove APPLE code.

--- es-app/src/VolumeControl.h
+++ es-app/src/VolumeControl.h
@@ -4,29 +4,22 @@
 
 #include <memory>
 
-#if defined (__APPLE__)
-#error TODO: Not implemented for MacOS yet!!!
-#elif defined(__linux__)
 #include <unistd.h>
 #include <fcntl.h>
 #include <alsa/asoundlib.h>
-#endif
 
 /*!
 Singleton pattern. Call getInstance() to get an object.
 */
 class VolumeControl
 {
-#if defined (__APPLE__)
-#error TODO: Not implemented for MacOS yet!!!
-#elif defined(__linux__)
+
 	static const char * mixerName;
 	static const char * mixerCard;
 	int mixerIndex;
 	snd_mixer_t* mixerHandle;
 	snd_mixer_elem_t* mixerElem;
 	snd_mixer_selem_id_t* mixerSelemId;
-#endif
 
 	int originalVolume;
 	int internalVolume;
