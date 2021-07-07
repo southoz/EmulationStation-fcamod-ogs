daa790a676fbeb7a009f34698381a19d89c1fea5
JuanMiguelBG <bacojmbg@gmail.com>
2021-07-07 07:46:34 +0100

    Fixes #16 - Internationalization of the games filter labels.

--- es-app/src/guis/GuiGamelistFilter.cpp
+++ es-app/src/guis/GuiGamelistFilter.cpp
@@ -3,6 +3,9 @@
 #include "components/OptionListComponent.h"
 #include "views/UIModeController.h"
 #include "SystemData.h"
+#include "Log.h"
+#include "EsLocale.h"
+#include "utils/StringUtil.h"
 
 GuiGamelistFilter::GuiGamelistFilter(Window* window, SystemData* system) : GuiComponent(window), mMenu(window, _("FILTER GAMELIST BY")), mSystem(system)
 {
@@ -18,7 +21,7 @@ void GuiGamelistFilter::initializeMenu()
 	mFilterIndex = mSystem->getIndex(true);
 
 	ComponentListRow row;
-	
+
 	// show filtered menu
 	row.elements.clear();
 	row.addElement(std::make_shared<TextComponent>(mWindow, _("RESET ALL FILTERS"), ThemeData::getMenuTheme()->Text.font, ThemeData::getMenuTheme()->Text.color), true);
@@ -63,7 +66,7 @@ void GuiGamelistFilter::addFiltersToMenu()
 		std::map<std::string, int>* allKeys = (*it).allIndexKeys; // all possible filters for this type
 		std::string menuLabel = _((*it).menuLabel); // text to show in menu
 		std::shared_ptr< OptionListComponent<std::string> > optionList;
-		
+
 		// add filters (with first one selected)
 		ComponentListRow row;
 
@@ -71,7 +74,36 @@ void GuiGamelistFilter::addFiltersToMenu()
 		optionList = std::make_shared< OptionListComponent<std::string> >(mWindow, menuLabel, true);
 		for(auto it: *allKeys)
 		{
-			optionList->add(it.first, it.first, mFilterIndex->isKeyBeingFilteredBy(it.first, type));
+			std::string optionLabel;
+
+			switch(type)
+			{
+				case RATINGS_FILTER:
+				{
+					int stars = std::atoi( &(it.first.c_str()[0]) );
+					LOG(LogDebug) << "GuiGamelistFilter::addFiltersToMenu():84 --> RATINGS_FILTER: [stars = " << it.first[0] << ", int = " << std::to_string(stars) << ']';
+					char starsbuf[16];
+					snprintf(starsbuf, 16, EsLocale::nGetText("%i STAR", "%i STARS", stars).c_str(), stars);
+					optionLabel.append( starsbuf );
+					break;
+				}
+				case FAVORITES_FILTER:
+				case HIDDEN_FILTER:
+				case KIDGAME_FILTER:
+				{
+					optionLabel = _(it.first);
+					LOG(LogDebug) << "GuiGamelistFilter::addFiltersToMenu():95 --> FAVORITES, HIDDEN, KIDGAME FILTERS : [value= " << it.first << ", traslation= " << optionLabel << ']';
+					break;
+				}
+				default:
+				{
+					LOG(LogDebug) << "GuiGamelistFilter::addFiltersToMenu():100 --> GENRE, PLAYER, PUBDEV FILTERS : " << it.first;
+					optionLabel = it.first;
+				}
+			}
+
+			optionList->add(optionLabel, it.first, mFilterIndex->isKeyBeingFilteredBy(it.first, type));
+			LOG(LogDebug) << "GuiGamelistFilter::addFiltersToMenu():106 --> index [\"" << it.first << "\", " << std::to_string(it.second) << ']';
 		}
 		if (allKeys->size() > 0)
 			mMenu.addWithLabel(menuLabel, optionList);
